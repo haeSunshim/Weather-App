48fd50278264e95059b8d8b9fca073ba5a1c748e
haesun <haesunsw@gmail.com>
2020-05-17 14:30:40 +1000

    get weather data

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("Hey this is a hidden message");
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=ed734e6370c7cf39fbb4628074dfc8c1`;
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+        });
+    });
+  } else {
+    h1.textContent = "Allow your location to see the accurate weather info";
+  }
+});
