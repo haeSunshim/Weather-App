bdd59999221926f469ddf75fe68824a08f3bba9a
haesun <haesunsw@gmail.com>
2020-05-17 15:03:58 +1000

    converted kelvin to celsius

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 window.addEventListener("load", () => {
   let long;
   let lat;
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -14,6 +20,9 @@ window.addEventListener("load", () => {
         })
         .then((data) => {
           console.log(data);
+          const { temp } = data.main;
+          // Set DOM Elements from the API
+          temperatureDegree.textContent = Number(temp - 273.15).toFixed(2);
         });
     });
   } else {
